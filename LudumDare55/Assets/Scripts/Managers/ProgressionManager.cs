907ac6559084b173cade55195f313fe3a380c06d
Bussler <maarten.bussler@gmail.com>
2024-04-13 16:46:35 +0200

    progression manager

---
+++ LudumDare55/Assets/Scripts/Managers/ProgressionManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manage progression in a bitflag.
+/// Implement a singleton pattern.
+/// </summary>
+public class ProgressionManager : MonoBehaviour
+{
+
+    public static ProgressionManager Instance;
+
+    [Flags]
+    public enum StoryFlags
+    {
+        None = 0,
+        Flag1 = 1 << 0,  // 1
+        Flag2 = 1 << 1,  // 2
+        Flag3 = 1 << 2,  // 4
+        Flag4 = 1 << 3,  // 8
+        Flag5 = 1 << 4,
+        Flag6 = 1 << 5,
+        Flag7 = 1 << 6,
+        Flag8 = 1 << 7
+    }
+
+    private StoryFlags storyFlags = StoryFlags.None;
+
+    public StoryFlags StoryFlags1 { get => storyFlags; set => storyFlags = value; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        storyFlags = StoryFlags.None;
+
+        SetStoryFlag(StoryFlags.Flag1);
+        SetStoryFlag(StoryFlags.Flag3);
+        var is_set = CheckStoryFlag(StoryFlags.Flag2);
+    }
+
+    /// <summary>
+    /// Set a flag in the progression.
+    /// </summary>
+    /// <param name="flag"></param>
+    public void SetStoryFlag(StoryFlags flag)
+    {
+        storyFlags |= flag;
+    }
+
+    /// <summary>
+    /// Check if a flag is set in the progression.
+    /// </summary>
+    /// <param name="flag"></param>
+    /// <returns></returns>
+    public bool CheckStoryFlag(StoryFlags flag)
+    {
+        return (storyFlags & flag) == flag;
+    }
+
+}
