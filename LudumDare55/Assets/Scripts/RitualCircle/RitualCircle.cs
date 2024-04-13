8019d4364478ddca33d035817fa286ff77169d31
Raoul Zebisch <neroware@freenet.de>
2024-04-13 23:10:39 +0200

    Ritual circle validation function

--- LudumDare55/Assets/Scripts/RitualCircle/RitualCircle.cs
+++ LudumDare55/Assets/Scripts/RitualCircle/RitualCircle.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UniRx;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Splines;
 
 public class RitualCircle : MonoBehaviour
@@ -14,6 +15,7 @@ public class RitualCircle : MonoBehaviour
     [SerializeField] private SplineTrailGenerator splineTrailGenerator;
 
     [SerializeField] private GameObject unitSphere;
+    [SerializeField] private float minCircleSurface = 30.0f;
 
     IObservable<Tuple<Vector3, float>> RitualCircleCompleted => ritualCircleCompleted;
 
@@ -25,9 +27,16 @@ public class RitualCircle : MonoBehaviour
                 Spline spline = splineTrailGenerator.Spline.Spline;
                 var circleKnots = spline.Knots.TakeLast(spline.Count - intersection).Select(x => x.Position).ToArray();
                 var center = CalculateCenter(circleKnots);
-                Vector3 v = (spline.Knots.First().Position - center);
-                return new Tuple<Vector3, float>(center, v.magnitude);
-            });
+                float r = 0.0f;
+                foreach (var knot in circleKnots) {
+                    Vector3 v = knot - center;
+                    r += v.magnitude;
+                }
+                r /= circleKnots.Length;
+                return new Tuple<float3[], Vector3, float>(circleKnots, center, r);
+            })
+            .Where(x => ValidateCircle(x.Item1, x.Item2))
+            .Select(x => new Tuple<Vector3, float>(x.Item2, x.Item3));
         
         ritualCircleCompleted.Subscribe(OnRitualCircleCompleted).AddTo(this);
     }
@@ -39,6 +48,28 @@ public class RitualCircle : MonoBehaviour
     private void OnRitualCircleCompleted(Tuple<Vector3, float> circle) {
         unitSphere.transform.position = circle.Item1;
         splineTrailGenerator.ResetSpline();
+    }
+
+
+    private bool ValidateCircle(float3[] points, Vector3 m) {
+        // Ensure at least three points are provided to form a polygon
+        if (points.Length < 3)
+        {
+            throw new Exception("Should not happen!");
+        }
+
+        // Calculate the surface area using the Shoelace formula
+        float area = 0f;
+        int numPoints = points.Length;
+        for (int i = 0; i < numPoints; i++)
+        {
+            Vector3 currentPoint = points[i];
+            Vector3 nextPoint = points[(i + 1) % numPoints];
+            area += Math.Abs(currentPoint.x * nextPoint.y - nextPoint.x * currentPoint.y);
+        }
+
+        // The absolute value of the sum divided by 2 is the surface area
+        return Mathf.Abs(area / 2f) > minCircleSurface;
     }
 
     private static float3 CalculateCenter(float3[] points) {
