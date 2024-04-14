b66afda4b797fb4f9daad952c75c7f3577ee228a
Luca Hohmann <hohmann.luca@tutanota.com>
2024-04-14 13:05:20 +0300

    Added Prod scene

--- LudumDare55/Assets/Scripts/GunSystem/ShootingComponent.cs
+++ LudumDare55/Assets/Scripts/GunSystem/ShootingComponent.cs
@@ -1,10 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Progress;
 
 public class ShootingComponent : MonoBehaviour
 {
