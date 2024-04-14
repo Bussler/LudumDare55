9d98273b5405680516d7766eccaf7bf57d03cf29
Raoul Zebisch <neroware@freenet.de>
2024-04-14 11:28:55 +0200

    Wrong variable used for time score interval

--- LudumDare55/Assets/Scripts/Score/TimeScore.cs
+++ LudumDare55/Assets/Scripts/Score/TimeScore.cs
@@ -18,7 +18,7 @@ public class TimeScore : MonoBehaviour
     void Awake() {
         timePointState = new ReactiveProperty<int>(totalTimePoints);
 
-        Observable.Interval(TimeSpan.FromSeconds(decreasePerInterval))
+        Observable.Interval(TimeSpan.FromSeconds(decreaseIntervalSec))
             .Subscribe(_ => OnTimeScoreTick())
             .AddTo(this);
         
